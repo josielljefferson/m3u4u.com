5035f57f7653ed4a3fb9b8c0527404459abd3372
josielljefferson <141317198+josielljefferson@users.noreply.github.com>
2024-12-10 06:09:27 -0300

    Update xe47yz1pd9spv21mn9vq.py

--- xe47yz1pd9spv21mn9vq.py
+++ xe47yz1pd9spv21mn9vq.py
@@ -1,19 +1,15 @@
 import requests
 import xml.etree.ElementTree as ET
-import logging
-
-# Configurar logs
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def download_epg(url, output_file):
     try:
-        response = requests.get(url, timeout=10)
+        response = requests.get(url)
         response.raise_for_status()  # Verifica se houve algum erro na solicitação
         with open(output_file, 'wb') as file:
             file.write(response.content)
-        logging.info(f"EPG atualizado e salvo em {output_file}")
+        print(f"EPG atualizado e salvo em {output_file}")
     except requests.exceptions.RequestException as e:
-        logging.error(f"Erro ao baixar o EPG: {e}")
+        print(f"Erro ao baixar o EPG: {e}")
 
 def update_epg():
     epg_url = "http://m3u4u.com/epg/xe47yz1pd9spv21mn9vq"  # Substitua pela URL do EPG
@@ -21,4 +17,5 @@ def update_epg():
     download_epg(epg_url, output_file)
 
 if __name__ == "__main__":
+    # Executa a atualização do EPG
     update_epg()
